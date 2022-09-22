7b1bc9f82109a57791690dca6dbef418e102e072
jamkerr <kerr0088@gmail.com>
2022-09-22 13:16:47 +0200

    Feat 1.5: Wrap array in IIFE with get and add funcitons

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,48 @@
-let pokemonList = [
-    {
-        name: "Diglett",
-        height: 0.08,
-        types: ['ground']
-    },
-    {
-        name: "Lickitung",
-        height: 3.11,
-        types: ['normal']
-    },
-    {
-        name: "Farfetch\'d",
-        height: 2.07,
-        types: ['normal', 'flying']
-    },
-    {
-        name: "Jigglypuff",
-        height: 1.08,
-        types: ['normal', 'fairy']
-    },
-    {
-        name: "Oddish",
-        height: 1.08,
-        types: ['grass', 'poison']
+let pokemonRespository = (function() {
+    let pokemonList = [
+        {
+            name: "Diglett",
+            height: 0.08,
+            types: ['ground']
+        },
+        {
+            name: "Lickitung",
+            height: 3.11,
+            types: ['normal']
+        },
+        {
+            name: "Farfetch\'d",
+            height: 2.07,
+            types: ['normal', 'flying']
+        },
+        {
+            name: "Jigglypuff",
+            height: 1.08,
+            types: ['normal', 'fairy']
+        },
+        {
+            name: "Oddish",
+            height: 1.08,
+            types: ['grass', 'poison']
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
     }
-];
+
+    function add(newPokemon) {
+        return pokemonList.push(newPokemon)
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
 
 // Writes the names and heights of each pokémon to the DOM
-pokemonList.forEach(function(pokemon) {
+pokemonRespository.getAll().forEach(function(pokemon) {
     if (pokemon.height > 3) {
         document.write(`<p>${pokemon.name} (height: ${pokemon.height}) - <em>Crikey, that's a whopper!</em></p>`);
     } else {
